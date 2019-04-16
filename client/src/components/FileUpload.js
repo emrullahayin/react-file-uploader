cf3cf60b75ac2f206f1a31ff78d94314931441df
Emrullah <emrullahayin@gmail.com>
2019-04-16 23:23:51 +0300

    view and alert

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from "react";
+import Message from "./Message";
 import axios from "axios";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState("");
 
   const onChange = e => {
     setFile(e.target.files[0]);
@@ -24,17 +26,19 @@ const FileUpload = () => {
       });
       const { fileName, filePath } = res.data;
       setUploadedFile({ fileName, filePath });
+      setMessage("File Uploaded");
     } catch (err) {
       if (err.response.status === 500) {
-        console.log("There was a pronlem with the server");
+        setMessage("There was a pronlem with the server");
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
   };
 
   return (
     <Fragment>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className="custom-file">
           <input
@@ -54,6 +58,17 @@ const FileUpload = () => {
           className="btn btn-primary btn-block mt-4"
         />
       </form>
+      {uploadedFile ? (
+        <div className="row mt-5">
+          <div className="col-md-6 m-auto">
+            <img
+              style={{ width: "100%" }}
+              src={uploadedFile.filePath}
+              alt={uploadedFile.fileName}
+            />
+          </div>
+        </div>
+      ) : null}
     </Fragment>
   );
 };
