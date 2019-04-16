f21e6627b268e2bfab34ddd495639f9a9f269d65
Emrullah Ayın <e.ayin@jollytur.com>
2019-04-16 16:25:51 +0300

    server.js config

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-
 const app = express();
 
+// default options
 app.use(fileUpload());
 
 // Upload Endpoint
 app.post("/upload", (req, res) => {
-  if (req.files === null) {
+  if (req.files === null)
     return res.status(400).json({ msg: "No file uploaded" });
-  }
 
   const file = req.files.file;
 
   file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
     if (err) {
-      console.log("error :", error);
+      console.error(error);
       return res.status(500).send(err);
     }
 
